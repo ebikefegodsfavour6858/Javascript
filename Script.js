8b605b39018d6b87c2754cdf72b4dc0329e3d343
ebikefegodsfavour6858 <ebikefegodsfavour6858@gmail.com>
2026-05-18 12:04:06 +0100

    feat: complete day 1 profile builder

---
+++ Script.js
@@ -0,0 +1,6 @@
+let string = "Perez"
+let age = 18;
+let iscodingbeginniner = true;
+let ageInFiveYears = age + 5 
+console.log(`Hi, my name is  ${string}. In five years, I will be  ${ageInFiveYears} years old.`)
+console.log(ageInFiveYears);
